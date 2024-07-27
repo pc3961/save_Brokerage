13065e7e5ceb5f5879b378442a91791fc8652a05
PC3961 <cpranavss65@gmail.com>
2024-07-27 16:22:13 +0530

    Added captcha and SMTP Server Setup

--- captcha.php
+++ captcha.php
@@ -1,4 +1,46 @@
 <?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '/mail/Exception.php';
+require '/mail/SMTP.php';
+require '/mail/PHPMailer.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'cgs@cgstechlab.com';                     //SMTP username
+    $mail->Password   = 'pwupgeqllrsfgxpz';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('', 'Contact Form');
+    $mail->addAddress('test@omesacreative.ca', 'Omesa Preview');     //Add a recipient
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $recaptcha_secret = '6LfIzRUqAAAAAPzljJhdvWZBkWtPov-9i0ouXn_q'; // Replace with your actual secret key
     $recaptcha_response = $_POST['g-recaptcha-response'];
